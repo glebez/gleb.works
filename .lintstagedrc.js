f034784d210297661062e51c281afd90f743aecb
glebez <glebkost.dev@gmail.com>
2021-11-02 17:06:27 +0100

    use next linting

---
+++ .lintstagedrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  '**/*.js?(x)': [
+    filenames =>
+      `next lint --fix --file ${filenames
+        .map(file => file.split(process.cwd())[1])
+        .join(' --file ')}`,
+    'prettier --write',
+    'git add',
+  ],
+};
