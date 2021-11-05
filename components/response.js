1b92f27e09b82e9bb1e8a31d3ca505c748c6e785
glebez <glebkost.dev@gmail.com>
2021-11-05 16:27:47 +0100

    rewrite response component to use hooks

--- components/response.js
+++ components/response.js
@@ -1,81 +1,85 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 
-class Response extends React.Component {
-  constructor(props) {
-    super(props);
+const Response = ({
+  onClick,
+  isActive,
+  text,
+  markActive,
+  id,
+  isMuted,
+  isKeyboardActivated,
+}) => {
+  const buttonElement = React.createRef();
 
-    this.buttonElement = React.createRef();
-    this.onClick = this.onClick.bind(this);
-  }
-  componentDidUpdate() {
-    const { isActive, isKeyboardActivated } = this.props;
-    if (this.buttonElement.current && isActive && isKeyboardActivated) {
-      this.buttonElement.current.scrollIntoView();
-    }
-  }
-  onClick(e) {
+  useEffect(
+    () => {
+      if (buttonElement.current && isActive && isKeyboardActivated) {
+        buttonElement.current.scrollIntoView();
+      }
+    },
+    [isActive, isKeyboardActivated]
+  );
+
+  const handleClick = e => {
     e.stopPropagation();
-    this.props.onClick();
-  }
+    onClick();
+  };
 
-  render() {
-    const { onClick, isActive, text, markActive, id, isMuted } = this.props;
-    const className = `response ${isActive ? 'active' : ''} ${
-      isMuted ? 'muted' : ''
-    }`;
-    return (
-      <React.Fragment>
-        <button
-          className={className}
-          onClick={this.onClick}
-          onMouseEnter={markActive.bind(null, id, false)}
-          ref={this.buttonElement}
-        >
-          {text}
-        </button>
-        <style jsx>{`
+  const className = `response ${isActive ? 'active' : ''} ${
+    isMuted ? 'muted' : ''
+  }`;
+  return (
+    <React.Fragment>
+      <button
+        className={className}
+        onClick={handleClick}
+        onMouseEnter={markActive.bind(null, id, false)}
+        ref={buttonElement}
+      >
+        {text}
+      </button>
+      <style jsx>{`
+        .response {
+          display: flex;
+          font-size: 28px;
+          color: #cffee5;
+          padding: 0;
+          margin-bottom: 10px;
+          margin-top: 0;
+          background-color: transparent;
+          font-family: inherit;
+          border: none;
+          cursor: pointer;
+          text-align: left;
+        }
+
+        .response:before {
+          content: '- ';
+          min-width: 30px;
+        }
+
+        .muted {
+          color: #998b9c;
+        }
+
+        .active {
+          color: #11fb7f;
+        }
+
+        .active:before {
+          content: url('/arrow.png');
+          font-size: 0;
+        }
+        @media screen and (max-width: 500px) {
           .response {
-            display: flex;
-            font-size: 28px;
-            color: #cffee5;
-            padding: 0;
-            margin-bottom: 10px;
-            margin-top: 0;
-            background-color: transparent;
-            font-family: inherit;
-            border: none;
-            cursor: pointer;
-            text-align: left;
+            font-size: 24px;
           }
-
-          .response:before {
-            content: '- ';
-            min-width: 30px;
-          }
-
-          .muted {
-            color: #998b9c;
-          }
-
-          .active {
-            color: #11fb7f;
-          }
-
-          .active:before {
-            content: url('/arrow.png');
-            font-size: 0;
-          }
-          @media screen and (max-width: 500px) {
-            .response {
-              font-size: 24px;
-            }
-          }
-        `}</style>
-      </React.Fragment>
-    );
-  }
-}
+        }
+      `}</style>
+    </React.Fragment>
+  );
+};
 
 Response.propTypes = {
   onClick: PropTypes.func,
