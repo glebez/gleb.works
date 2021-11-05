5fc811b1620fa2143ca2d01eb4551a8621bf4aeb
glebez <glebkost.dev@gmail.com>
2021-11-05 14:06:46 +0100

    rewrite messages component to use hooks

--- components/messages.js
+++ components/messages.js
@@ -1,66 +1,58 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Message from './message';
 
-class Messages extends React.Component {
-  constructor(props) {
-    super(props);
+const Messages = ({ messages, showNext }) => {
+  const scrollableContainer = React.createRef();
 
-    this.scrollableContainer = React.createRef();
-    this.scrollToBottom = this.scrollToBottom.bind(this);
-  }
-
-  componentDidUpdate() {
-    this.scrollToBottom();
-  }
-
-  scrollToBottom() {
-    if (this.scrollableContainer.current) {
-      this.scrollableContainer.current.scrollTo(
+  const scrollToBottom = () => {
+    if (scrollableContainer.current) {
+      scrollableContainer.current.scrollTo(
         0,
-        this.scrollableContainer.current.scrollHeight
+        scrollableContainer.current.scrollHeight
       );
     }
-  }
+  };
 
-  render() {
-    const { messages, showNext } = this.props;
-    return (
-      <div className="messages-container" ref={this.scrollableContainer}>
-        <div className="messages-container__inner">
-          {messages &&
-            messages.map(({ text, author }, i) => (
-              <Message
-                text={text}
-                author={author}
-                key={`${text}-${i}`}
-                showNext={showNext}
-                scrollToBottom={this.scrollToBottom}
-              />
-            ))}
-        </div>
-        <style jsx>{`
-          .messages-container {
-            grid-column-start: 2;
-            overflow-y: scroll;
-            align-self: end;
-            height: 100%;
-            padding: 20px;
-            padding-top: 40px;
-            border-bottom: 5px solid #58066b;
-          }
+  useEffect(() => {
+    scrollToBottom();
+  }, []);
 
-          .messages-container__inner {
-            display: flex;
-            flex-direction: column;
-            justify-content: flex-end;
-            min-height: 100%;
-          }
-        `}</style>
+  return (
+    <div className="messages-container" ref={scrollableContainer}>
+      <div className="messages-container__inner">
+        {messages &&
+          messages.map(({ text, author }, i) => (
+            <Message
+              text={text}
+              author={author}
+              key={`${text}-${i}`}
+              showNext={showNext}
+              scrollToBottom={scrollToBottom}
+            />
+          ))}
       </div>
-    );
-  }
-}
+      <style jsx>{`
+        .messages-container {
+          grid-column-start: 2;
+          overflow-y: scroll;
+          align-self: end;
+          height: 100%;
+          padding: 20px;
+          padding-top: 40px;
+          border-bottom: 5px solid #58066b;
+        }
+
+        .messages-container__inner {
+          display: flex;
+          flex-direction: column;
+          justify-content: flex-end;
+          min-height: 100%;
+        }
+      `}</style>
+    </div>
+  );
+};
 
 Messages.propTypes = {
   messages: PropTypes.array,
