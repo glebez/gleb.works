b45737ff72017e03364c36366e97a84cd6ce8887
glebez <glebkost.dev@gmail.com>
2021-11-05 14:01:15 +0100

    rewrite chat component to use hooks

--- components/chat.js
+++ components/chat.js
@@ -1,107 +1,93 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Avatar from './avatar';
 import ChatBrain from '../chat';
 import Message from '../chat/message';
 import Messages from './messages';
 import Responses from './responses.js';
 
-class Chat extends React.Component {
-  constructor(props) {
-    super(props);
-    this.chat = new ChatBrain();
-    this.state = {
-      messages: [],
-      shownMessages: [],
-      responses: [],
-      chosenResponseIds: [],
-    };
+const Chat = () => {
+  const chat = new ChatBrain();
+  const [messages, setMessages] = useState([]);
+  const [shownMessages, setShownMessages] = useState([]);
+  const [responses, setResponses] = useState([]);
+  const [chosenResponseIds, setChosenResponsesIds] = useState([]);
+  const [isDoneTyping, setIsDoneTyping] = useState(false);
+  const shouldShowNext = useRef(true);
 
-    this.showNext = this.showNext.bind(this);
-    this.handleResponse = this.handleResponse.bind(this);
-  }
-
-  componentDidMount() {
-    this.handleResponse('welcome');
-  }
-
-  showNext() {
-    const { messages, shownMessages } = this.state;
+  const showNext = () => {
     if (!messages.length) {
-      this.setState({ isDoneTyping: true });
+      setIsDoneTyping(true);
     } else {
       const [next, ...rest] = messages;
-      this.setState({
-        isDoneTyping: false,
-        messages: rest,
-        shownMessages: [...shownMessages, next],
-      });
+      setIsDoneTyping(false);
+      setMessages(rest);
+      setShownMessages([...shownMessages, next]);
     }
-  }
+  };
 
-  handleResponse(value, text, id) {
-    const { messages, responses } = this.chat.trigger(value);
-    const {
-      shownMessages: oldShownMessages,
-      chosenResponseIds: oldChosenResponseIds,
-    } = this.state;
-    const shownMessages = text
-      ? [...oldShownMessages, new Message(text, 'user')]
-      : oldShownMessages;
-    const chosenResponseIds = [...oldChosenResponseIds, id];
-    this.setState(
-      {
-        messages,
-        shownMessages,
-        responses,
-        chosenResponseIds,
-      },
-      () => this.showNext()
-    );
-  }
+  const handleResponse = (value, text, id) => {
+    const { messages, responses } = chat.trigger(value);
+    const newShownMessages = text
+      ? [...shownMessages, new Message(text, 'user')]
+      : shownMessages;
+    const newChosenResponseIds = [...chosenResponseIds, id];
+    shouldShowNext.current = true;
+    setMessages(messages);
+    setResponses(responses);
+    setShownMessages(newShownMessages);
+    setChosenResponsesIds(newChosenResponseIds);
+  };
 
-  render() {
-    const {
-      isDoneTyping,
-      shownMessages,
-      responses,
-      chosenResponseIds,
-    } = this.state;
-    return (
-      <div className="chat">
-        <div className="chat__inner">
-          <Avatar />
-          <Messages messages={shownMessages} showNext={this.showNext} />
-          {isDoneTyping && (
-            <Responses
-              responses={responses}
-              chosenResponseIds={chosenResponseIds}
-              handleResponse={this.handleResponse}
-              markResponseActive={this.markResponseActive}
-            />
-          )}
-        </div>
-        <style jsx>{`
-          .chat {
-            position: relative;
-          }
-          .chat__inner {
-            display: grid;
-            grid-template-columns: 1fr 9fr;
-            grid-template-rows: 2fr 1fr;
-            height: 100vh;
-            max-width: 900px;
-            margin: 0 auto;
-          }
+  useEffect(
+    () => {
+      shouldShowNext.current && showNext();
+      return () => {
+        if (messages.length) {
+          shouldShowNext.current = false;
+        }
+      };
+    },
+    [messages]
+  );
 
-          @media screen and (max-width: 768px) {
-            .chat__inner {
-              grid-template-columns: 1fr;
-            }
-          }
-        `}</style>
+  useEffect(() => {
+    handleResponse('welcome');
+  }, []);
+
+  return (
+    <div className="chat">
+      <div className="chat__inner">
+        <Avatar />
+        <Messages messages={shownMessages} showNext={showNext} />
+        {isDoneTyping && (
+          <Responses
+            responses={responses}
+            chosenResponseIds={chosenResponseIds}
+            handleResponse={handleResponse}
+          />
+        )}
       </div>
-    );
-  }
-}
+      <style jsx>{`
+        .chat {
+          position: relative;
+        }
+        .chat__inner {
+          display: grid;
+          grid-template-columns: 1fr 9fr;
+          grid-template-rows: 2fr 1fr;
+          height: 100vh;
+          max-width: 900px;
+          margin: 0 auto;
+        }
+
+        @media screen and (max-width: 768px) {
+          .chat__inner {
+            grid-template-columns: 1fr;
+          }
+        }
+      `}</style>
+    </div>
+  );
+};
 
 export default Chat;
