f5f087c3f4910b4e936b23b39f336ebcc9a5c0f0
glebez <glebkost.dev@gmail.com>
2021-11-05 14:06:29 +0100

    chage Avatar to functional component

--- components/avatar.js
+++ components/avatar.js
@@ -1,29 +1,27 @@
 import React from 'react';
 
-class Avatar extends React.Component {
-  render() {
-    return (
-      <React.Fragment>
-        <img className="avatar" src="/avatar.jpeg" alt="Gleb Kost" />
-        <style jsx>{`
-          .avatar {
-            height: 100px;
-            margin-right: 40px;
-            border-image: url('/border.png') 20 28 / 18px 32px / 15px round;
-            border-style: solid;
-            margin-bottom: 25px;
-            align-self: end;
-          }
+const Avatar = () => {
+  return (
+    <React.Fragment>
+      <img className="avatar" src="/avatar.jpeg" alt="Gleb Kost" />
+      <style jsx>{`
+        .avatar {
+          height: 100px;
+          margin-right: 40px;
+          border-image: url('/border.png') 20 28 / 18px 32px / 15px round;
+          border-style: solid;
+          margin-bottom: 25px;
+          align-self: end;
+        }
 
-          @media screen and (max-width: 768px) {
-            .avatar {
-              display: none;
-            }
+        @media screen and (max-width: 768px) {
+          .avatar {
+            display: none;
           }
-        `}</style>
-      </React.Fragment>
-    );
-  }
-}
+        }
+      `}</style>
+    </React.Fragment>
+  );
+};
 
 export default Avatar;
