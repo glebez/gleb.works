886ed3df2e78dd28f39a549c80ed012978d2d0bb
Gleb Kostyunin <gleb.kostyunin@sinnerschrader.com>
2022-02-25 11:52:25 +0100

    update info

--- chat/scenes/contacts.js
+++ chat/scenes/contacts.js
@@ -9,7 +9,7 @@ export default function contacts(state) {
   }
   messages = [
     ...messages,
-    "The best way to contact me with work or consulting opportunities is by shooting an email to [glebkost.dev@gmail.com](mailto:gelbkost.dev@gmail.com).\n\nThere's also a phone number [+4915204246147](tel:+4915204246147), but I really discourage you to call me, unless we are aquainted or agreed on having a call in advance.\n\nAnd in case you are email allergic, but still want to contact me, you can use the phone number above to look me up in the telegram messenger.",
+    "The best way to contact me with work or consulting opportunities is by shooting an email to [glebkost.dev@gmail.com](mailto:gelbkost.dev@gmail.com).\n\nThere's also a phone number [+498920174044](tel:+498920174044), but I really discourage you to call me, unless we are aquainted or agreed on having a call in advance.\n\nAnd in case you are email allergic, but still want to contact me, you can look me up in the telegram messenger as @glebkost.",
   ];
 
   return {
--- chat/scenes/profIntro.js
+++ chat/scenes/profIntro.js
@@ -12,7 +12,7 @@ export default function profIntro(state) {
   }
   messages = [
     ...messages,
-    'I am a web developer with 6+ years of experience. Throughout my career I was mostly focusing on frontend development, with a heavy emphasis on javascript, although I love css and html with all my heart.',
+    'I am a web developer with 7 years of experience. Throughout my career I was mostly focusing on frontend development, with a heavy emphasis on javascript, although I love css and html with all my heart.',
     "First 4 years of my career were rotating around react and I've stretched over to cover full stack with node and express. Expo and React Native gave me a chance to successfully pretend I can do mobile development and at some point there even were a working business app in AppStore that I've built for one of my clients. Unfortunately, it is no longer listed.",
     'In the last 3 years being in a digital agency environment I had luck working with all major frontend development frameworks. While react is still my pick of choice, I see unique benefits in each of them.',
   ];
