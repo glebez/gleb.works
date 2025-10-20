3d1e038c5ce6f6ab273a72c66299e948365aceea
Gleb Kostyunin <glebkost.dev@gmail.com>
2025-10-20 16:06:08 +0200

    update CV

--- src/brains/scenes/profCareer.ts
+++ src/brains/scenes/profCareer.ts
@@ -7,7 +7,7 @@ export default function profCareer(): SceneReturn {
     'I have experience working in all sorts of companies from small fast-paced startups to huge transnational corporations.',
     'My longest collaboration was with Sinnerschrader digital agency, where I was employed till March 2022. My total run there was more than 5 years. Right now I am focusing on contract work.',
     'Here are some well-known brand names that I wrote code for: ADAC, Allianz, VW, AUDI, FC Bayern, Harrods, Telefonica (o2), About You, Merk.',
-    'For more detailed look you can download my CV [here](/assets/Gleb_Kostyunin_CV_07_2025.pdf).',
+    'For more detailed look you can download my CV [here](/assets/Gleb_Kostyunin_CV_10_2025.pdf).',
   ].map(text => new Message(text));
 
   return {
